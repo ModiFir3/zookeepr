eb1a1230432a5884ecde6866ac330c94de7580da
PaulS <psoliz333@gmail.com>
2022-04-12 17:26:14 -0500

    added insomnia to test code and post

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const res = require('express/lib/response');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+//parse incoming JSON data
+app.use(express.json())
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -64,6 +72,29 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404)
   }
+});
+
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  
+  animalsArray.push(animal);
+
+  fs.writeFileSync(path.join(__dirname, './data/animals.json'),
+  JSON.stringify({ animals: animalsArray }, null, 2)
+  )
+
+  //return finishes code to post route for reponse
+  return body;
+}
+
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  //add animal to jason file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+
+  res.json(req.body);
 })
 
 app.listen(PORT, () => {
