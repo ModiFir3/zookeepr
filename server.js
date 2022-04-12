33d40e9724ec1099f574914c76b918c6a075ccb0
PaulS <psoliz333@gmail.com>
2022-04-12 15:41:50 -0500

    created input for requested data

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
@@ -31,7 +32,7 @@ function filterByQuery(query, animalsArray) {
     });
   }
   if (query.diet) {
-    
+
     filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
   }
   if (query.species) {
@@ -43,6 +44,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -50,6 +56,15 @@ app.get('/api/animals', (req, res) => {
   }
   res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result)
+  } else {
+    res.send(404)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
