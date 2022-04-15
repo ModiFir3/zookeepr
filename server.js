92dfd916457dc20a4ab4040165fca7939652d2df
PaulS <psoliz333@gmail.com>
2022-04-15 02:32:06 -0500

    tested files for animals and added main files to zookeepers

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const { animals } = require('./data/animals');
-// const res = require('express/lib/response');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,24 +8,12 @@ const app = express();
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
-app.use(express.json());
 app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
